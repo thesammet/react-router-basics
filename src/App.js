33142d61ef172276925625cdc55be83dd9dc322b
Abdü Samed AKGÜL <50338990+thesammet@users.noreply.github.com>
2023-02-16 01:11:56 +0300

    setup react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
+import { BrowserRouter, Routes, Route, Link, NavLink } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+
 function App() {
   return (
-    <div className="App">
-      Hello, World!
-    </div>
+    <BrowserRouter>
+      <header>
+        <nav>
+          <h1>Basic Router</h1>
+          <NavLink to='/'>Home</NavLink>
+          <NavLink to='about'>About</NavLink>
+        </nav>
+      </header>
+      <main>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path='about' element={<About />} />
+        </Routes>
+      </main>
+    </BrowserRouter>
   );
 }
 
