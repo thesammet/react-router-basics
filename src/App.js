cfe2a74edb55420dfcd966655f8121a3a8f159db
Abdü Samed AKGÜL <50338990+thesammet@users.noreply.github.com>
2023-02-19 21:15:38 +0300

    addition data handle on form application

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import RootLayout from './layouts/RootLayout';
 import HelpLayout from './layouts/HelpLayout';
 import CareersLayout from './layouts/CareersLayout';
 import Faq from './pages/help/Faq'
-import Contact from './pages/help/Contact'
+import Contact, { contactAction } from './pages/help/Contact'
 import Error from './pages/Error';
 import Careers, { careersDataLoader } from './pages/careers/Careers';
 import CareerDetails, { careerDetailsLoader } from './pages/careers/CareerDetails';
@@ -23,7 +23,7 @@ const router = createBrowserRouter(
       <Route path='about' element={<About />} />
       <Route path='help' element={<HelpLayout />}>
         <Route path='faq' element={<Faq />} />
-        <Route path='contact' element={<Contact />} />
+        <Route path='contact' element={<Contact />} action={contactAction} />
       </Route>
       <Route path='careers'
         element={<CareersLayout />}
--- src/pages/help/Contact.js
+++ src/pages/help/Contact.js
@@ -1,8 +1,13 @@
+import { Form, redirect, useActionData } from "react-router-dom";
+
 export default function Contact() {
+
+    const data = useActionData()
+
     return (
         <div className="contact">
             <h3>Contact Us</h3>
-            <form>
+            <Form method='post' action="/help/contact">
                 <label>
                     <span>Your email:</span>
                     <input type="email" name="email" required />
@@ -12,7 +17,26 @@ export default function Contact() {
                     <textarea name="message" required></textarea>
                 </label>
                 <button>Submit</button>
-            </form>
+                {data && data.error && <p>{data.error}</p>}
+            </Form>
         </div>
     )
+}
+
+export const contactAction = async ({ request }) => {
+    const data = await request.formData()
+
+    const submission = {
+        email: data.get('email'),
+        message: data.get('message')
+    }
+    console.log(submission.message.length)
+    if (submission.message.length < 10) {
+        return {
+            error: "Message length can't be less than 10."
+        }
+    }
+
+    return redirect('/')
+
 }
